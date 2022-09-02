a186538be815bc18d8d301dcb38022b52318ac97
Tanvir Ahmed <tanvir66288@gmai.com>
2022-09-02 20:05:44 +0600

    TA | added game runner with command for python agent

--- src/test/java/SkeletonMain.java
+++ src/test/java/SkeletonMain.java
@@ -1,29 +1,20 @@
 import com.codingame.gameengine.runner.MultiplayerGameRunner;
+import com.codingame.gameengine.runner.simulate.GameResult;
 
 public class SkeletonMain {
     public static void main(String[] args) {
-
-        // Uncomment this section and comment the other one to create a Solo Game
-        /* Solo Game */
-        // SoloGameRunner gameRunner = new SoloGameRunner();
-
-        // Sets the player
-        // gameRunner.setAgent(Player1.class);
-
-        // Sets a test case
-        // gameRunner.setTestCase("test1.json");
-
         /* Multiplayer Game */
         MultiplayerGameRunner gameRunner = new MultiplayerGameRunner();
 
         // Adds as many player as you need to test your game
-        gameRunner.addAgent(Agent1.class);
-        gameRunner.addAgent(Agent2.class);
-
-        // Another way to add a player
-        // gameRunner.addAgent("python3 /home/user/player.py");
-        
+//        gameRunner.addAgent("python3 /home/tanvir/IdeaProjects/agents/wordle_multiplayer/input_test.py");
+        gameRunner.addAgent("python3 /home/tanvir/IdeaProjects/agents/wordle_multiplayer/arena.py");
+        gameRunner.addAgent("python3 /home/tanvir/IdeaProjects/agents/wordle_multiplayer/random_word.py");
 
         gameRunner.start();
+//        GameResult result = gameRunner.simulate();
+
+//        System.out.println(result.summaries.toString());
+
     }
 }
