3fef9ef35efb5195b769920d38f9b99f17eae0f1
Tanvir Ahmed <tanvir66288@gmai.com>
2022-09-02 20:02:03 +0600

    TA | added java agents

--- src/test/java/Agent1.java
+++ src/test/java/Agent1.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Agent1 {
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Agent1 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int wordCount = in.nextInt();
+        String[] wordSet = new String[wordCount];
+
+        for (int i = 0; i < wordCount; i++) {
+            String word = in.next();
+            wordSet[i] = word;
+        }
+
+        System.err.println(String.join(" ", wordSet));
+
+        // game loop
+        while (true) {
+
+            int[] states = new int[5];
+
+            for (int i = 0; i < 5; i++) {
+                int state = in.nextInt();
+                states[i] = state;
+            }
+
+            System.err.println(String.join(" ", Arrays.toString(states).split("[\\[\\]]")[1].split(", ")));
+
+            // Write an answer using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("GUESS");
+        }
+    }
 }
--- src/test/java/Agent2.java
+++ src/test/java/Agent2.java
@@ -5,9 +5,19 @@ public class Agent2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        int guess = (start+end)/2;
+
         while (true) {
-            String input = scanner.nextLine();
-            System.out.println("my output");
+            int input = scanner.nextInt();
+            if (input == 1) end = guess - 1;
+            else if (input == -1) start = guess + 1;
+
+            guess = (start+end)/2;
+
+            System.out.println(guess);
         }
     }
 }
